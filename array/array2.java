aacc557b29dd3678714d8670ab4a2797dc15a709
Marcello <marcellokgnoctis92@gmail.com>
2023-06-14 09:11:34 +0200

    ercizio svolto Please enter the commit message for your changes. Lines starting

---
+++ array/array2.java
@@ -0,0 +1,17 @@
+package array;
+
+import java.util.ArrayList;
+
+public class array2 {
+    public static void main(String[] args) {
+        int[] primeNumbers = new int[6];
+        primeNumbers[0] = 1;
+        primeNumbers[1] = 2;
+        primeNumbers[2] = 3;
+        primeNumbers[3] = 4;
+        primeNumbers[4] = 5;
+        primeNumbers[5] = 6;
+        System.out.println(primeNumbers.length);
+
+    }
+}
