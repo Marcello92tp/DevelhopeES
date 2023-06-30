814796e0fa0bad13b74f5dc3de1887768fb8278f
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-06-30 11:55:48 +0200

    esercizio svolto

---
+++ encapsulation/House.java
@@ -0,0 +1,37 @@
+package encapsulation;
+//Encapsulation
+//        Achieve encapsulation using getters/setters for a class House that has the following instance variables:
+//        an int floorsNumber
+//        a string address
+//        an array of strings residentsNames
+//
+public class House {
+
+    private int floorsNumber;
+    private String address;
+    private String[] residentName;
+
+    public void setFloorsNumber(int floorsNumber) {
+        this.floorsNumber = floorsNumber;
+    }
+
+    public int getFloorsNumber() {
+        return floorsNumber;
+    }
+
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setResidentName(String[]residentName){
+        this.residentName = residentName;
+    }
+    public String[] getResidentName(){
+        return residentName;
+    }
+}
---
+++ encapsulation/testerhouse.java
@@ -0,0 +1,17 @@
+package encapsulation;
+// use a tester class to:
+//        set the values by asking the user to input them
+//        for the residentsNames, the user will have to input a list of names separated by a comma (e.g. John,Lucy,Peter)
+//        prints the house details using the getters and String.format()
+public class testerhouse {
+    public static void main(String[] args) {
+        String[] residents = {"Marcello, Cristian, Pietro"};
+        House house = new House();
+        house.setAddress("Via poggio 6");
+        house.setFloorsNumber(2);
+        house.setResidentName(residents);
+
+        String formatted = String.format("the house is %s has %d floors and is occuped by %s", house.getAddress(),house.getFloorsNumber(),String.join(",", house.getResidentName()));
+        System.out.println(formatted);
+    }
+}
