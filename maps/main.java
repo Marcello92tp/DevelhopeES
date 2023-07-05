54c2b8904bc2bd7635beaa3a4ce1ff08b8253f5e
Marcello92tp <134273546+Marcello92tp@users.noreply.github.com>
2023-07-05 11:55:58 +0200

    Update main.java

--- maps/main.java
+++ maps/main.java
@@ -25,14 +25,32 @@ public class main {
         numbers1.put(5, "Cinque");
         numbers1.put(7, "Sette");
         numbers1.put(9, "Nove");
+       
         for (Integer key : numbers1.keySet()) {
             numbers.put(key, numbers1.get(key));
         }
-        System.out.println("Key 5: " + numbers.getOrDefault(5, "senza valore associato."));
-        System.out.println("Key 6: " + numbers.getOrDefault(6, "senza valore associato."));
-        System.out.println("Key 7: " + numbers.getOrDefault(7, "senza valore associato."));
-        System.out.println("Key 8: " + numbers.getOrDefault(8, "senza valore associato."));
-        System.out.println("Key 9: " + numbers.getOrDefault(9, "senza valore associato."));
 
+        System.out.println("Modified HashMap:");
+        System.out.println(numbers);
+
+        System.out.println("Values for keys 5, 6, 7, 8, and 9:");
+
+        printValueForKey(numbers, 5);
+        printValueForKey(numbers, 6);
+        printValueForKey(numbers, 7);
+        printValueForKey(numbers, 8);
+        printValueForKey(numbers, 9);
     }
-}
+
+    public static void printValueForKey(HashMap<Integer, String> map, int key) {
+        String value = map.get(key);
+        if (value != null) {
+            System.out.println("Key: " + key + ", Value: " + value);
+        } else {
+            System.out.println("Key: " + key + ", No associated value");
+        }
+    }
+
+
+
+
