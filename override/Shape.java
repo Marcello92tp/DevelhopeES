ac50271ed6d1bcf8a790eccb25e7e5166cd2f442
Marcello <marcellokgnoctis92@gmail.com>
2023-07-24 13:03:18 +0200

    esercizio override completato

---
+++ override/Shape.java
@@ -0,0 +1,18 @@
+package override;
+
+public class Shape {
+
+    private String shapeName;
+
+    private Integer numberOfEdges;
+
+    public Shape(String shapeName, Integer numberOfEdges) {
+        this.shapeName = shapeName;
+        this.numberOfEdges = numberOfEdges;
+    }
+
+    public void getShapeDetails(){
+        System.out.println("Shape Name: " + shapeName + "\nNumber Of Edges: " + numberOfEdges);
+    }
+
+}
